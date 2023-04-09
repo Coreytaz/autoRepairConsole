e92de242d343d389df878f1006a64cdea90ee2cb
Coreytaz <coreytaz321@gmail.com>
2023-04-09 16:52:31 +0500

    fix button

--- src/shared/ui/button/index.tsx
+++ src/shared/ui/button/index.tsx
@@ -1,21 +1,20 @@
 import * as React from "react"
 import { VariantProps, cva } from "class-variance-authority"
- 
+
 import { cn } from "~shared/lib"
- 
+
 const buttonVariants = cva(
-  "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors dark:hover:bg-slate-800 dark:hover:text-slate-100 disabled:opacity-50  disabled:pointer-events-none data-[state=open]:bg-slate-100 dark:data-[state=open]:bg-slate-800",
+  "font-sans text-lg font-medium text-secondary flex cursor-pointer items-center gap-4 rounded-lg px-4 py-2",
   {
     variants: {
-      
       isActive: {
         true: "bg-green-300 text-primary"
       },
       variant: {
         default:
-        "bg-green-500 text-white hover:bg-green-300 rounded-2xl",
+          "bg-green-500 text-white hover:bg-green-300 rounded-2xl",
         ghost:
-        "text-secondary hover:bg-primaryLight hover:text-primary",
+          "hover:bg-primaryLight hover:text-primary",
       },
       size: {
         default: "h-10 py-2 px-4",
@@ -27,27 +26,34 @@ const buttonVariants = cva(
       variant: "default",
       size: "default",
     },
-    
+
   }
 
 )
- 
+
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {}
- 
+  VariantProps<typeof buttonVariants> {
+  icon: React.ReactNode
+}
+
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, isActive, size, ...props }, ref) => {
+  ({ className, variant, isActive, size, icon, children, ...props }, ref) => {
     return (
       <button
         className={cn(buttonVariants({ variant, isActive, size, className }))}
         ref={ref}
         {...props}
-      />
+      >
+        <span className={cn("[&>svg]:stroke-secondary", {
+          '[&>svg]:stroke-primary': isActive,
+        })}>{icon}</span>
+        {children}
+      </button>
     )
   }
 )
 
 Button.displayName = "Button"
- 
+
 export { Button, buttonVariants }
