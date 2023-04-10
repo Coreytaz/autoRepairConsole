16ff1921a7eb10285573125de9ac765f8c0452db
dunnodunno <quexnox@gmail.com>
2023-04-10 23:47:09 +0500

    history

--- src/pages/HistoryOrder/index.tsx
+++ src/pages/HistoryOrder/index.tsx
@@ -1,7 +1,46 @@
 import { FC } from 'react';
 
+import { Table } from "~shared/ui";
+import { Card, cardVariants } from "~shared/ui";
+import { Typography } from '~shared/ui';
+
 const HistoryOrderPage: FC = () => {
-  return <div>HistoryOrderPage</div>;
-};
+  return (
+    <div className='mt-7 mx-8'>
+       <div className='pb-6'  >
+           <Typography tag='h2' className="font-semibold">История заказов</Typography>    
+       </div>
+          <Card className='w-full h-full' variant='default'>
+    
+           <div className='px-7 pt-2 pb-7'>
+              <>
+              <Table className='' columns={columns} data={rows} isPagination pageIndex={0} pageSize={10} isSearch/>
+              </>
+          </div>
+          </Card>
+   </div>
+     
+    
+     
+     );
+   };
+
+   const columns = [
+    { accessor: "orderNumber", Header: "Заказ" },
+    { accessor: "request", Header: "Услуга" },
+    { accessor: "nameOfEmployee", Header: "ФИО Сотрудника" },
+    { accessor: "nameOfClient", Header: "ФИО Клиента" },
+    { accessor: "auto", Header: "Автомобиль" },
+    { accessor: "price", Header: "Цена" },
+  ];
+  
+  const rows = [
+    { key: "1", orderNumber: "1", request: "услуга", nameOfEmployee: "сотрудник 1", nameOfClient: "Клиент 1", auto: "автомобиль", price: "10000 р" },
+    { key: "2", orderNumber: "2", request: "услуга", nameOfEmployee: "сотрудник 2", nameOfClient: "Клиент 2", auto: "автомобиль", price: "14400 р" },
+    { key: "3", orderNumber: "3", request: "услуга", nameOfEmployee: "сотрудник 3", nameOfClient: "Клиент 3", auto: "автомобиль", price: "10300 р" },
+    { key: "4", orderNumber: "4", request: "услуга", nameOfEmployee: "сотрудник 2", nameOfClient: "Клиент 4", auto: "автомобиль", price: "12200 р" },
+    { key: "5", orderNumber: "5", request: "услуга", nameOfEmployee: "сотрудник 2", nameOfClient: "Клиент 5", auto: "автомобиль", price: "10780 р" },
+  ];
+
 
 export default HistoryOrderPage;
