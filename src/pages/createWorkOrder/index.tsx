0eeff0f6424c94c357c4a93738030b6a4f3b5336
Coreytaz <coreytaz321@gmail.com>
2023-04-17 23:51:12 +0500

    finish page createWorkOrder

--- src/pages/createWorkOrder/index.tsx
+++ src/pages/createWorkOrder/index.tsx
@@ -1,14 +1,18 @@
 import { FC } from "react"
 import { Steps } from "react-step-builder";
 
+import { FormProvider, useForm } from "react-hook-form";
+
 import { Card, Typography } from "~shared/ui";
 
 import { StepsProgress } from "~entities/ProgressStepsBar";
 
 import ClientInfo from "./clientInfo";
 import AvtoInfo from "./avtoInfo";
+import ServiceInfo from "./serviceInfo";
 
 const CreateWorkOrder: FC = () => {
+    const methods = useForm();
 
     return (
         <section className="mt-7 sm:mx-4 xl:mx-8">
@@ -17,12 +21,15 @@ const CreateWorkOrder: FC = () => {
             </div>
             <Card className='w-full h-full mt-8 p-5 text-center' variant='default'>
                 <StepsProgress />
-                <Steps>
-                    <ClientInfo />
-                    <AvtoInfo />
-                </Steps>
+                <FormProvider {...methods}>
+                    <Steps>
+                        <ClientInfo methods={methods} />
+                        <AvtoInfo methods={methods} />
+                        <ServiceInfo methods={methods} />
+                    </Steps>
+                </FormProvider>
             </Card>
-        </section>
+        </section >
     )
 }
 
---
+++ src/pages/createWorkOrder/serviceInfo/index.tsx
@@ -0,0 +1,78 @@
+import { FC, useMemo } from 'react'
+import { FieldValues, UseFormReturn, useForm } from 'react-hook-form';
+import { useSteps } from 'react-step-builder';
+
+import { BaseRadioField, BaseTextField, Button, Typography } from '~shared/ui';
+
+const ServiceInfo: FC<{ methods: UseFormReturn<FieldValues, any> }> = ({ methods }) => {
+    const { next, prev } = useSteps()
+
+    const onSubmit = (data: any) => {
+        next()
+        console.log(data)
+    }
+
+    const ServiceSelectionItem = useMemo(() => [
+        {
+            value: 'двигатель',
+            title: 'Двигатель'
+        },
+        {
+            value: 'услуга',
+            title: 'Услуга'
+        },
+    ], [])
+
+    const EmployeeItem = useMemo(() => [
+        {
+            value: 'Иванов-Иван-Иваныч',
+            title: 'Иванов Иван Иваныч'
+        },
+    ], [])
+
+    return (
+        <div>
+            <Typography tag="span" className="font-semibold uppercase text-2xl">УСЛУГА</Typography>
+            <form onSubmit={methods.handleSubmit(onSubmit)} className="mt-5 flex flex-col gap-5 items-center">
+                <div className='flex gap-3 flex-col max-w-lg w-full items-end'>
+                    <BaseRadioField
+                        className='h-10'
+                        variant='dark'
+                        selectItem={ServiceSelectionItem}
+                        labelName='Выбор услуги'
+                        name='ServiceSelection'
+                        required />
+                    <BaseTextField
+                        className='h-10'
+                        name="Model"
+                        type='text'
+                        required
+                        labelName='Стоимость'
+                        variant='dark'
+                    />
+                    <BaseRadioField
+                        className='h-10'
+                        variant='dark'
+                        selectItem={EmployeeItem}
+                        labelName='Сотрудник'
+                        name='Employee'
+                        required />
+                    <BaseTextField
+                        className='h-10'
+                        name="DateAndTimeOfIssue"
+                        required
+                        labelName='Дата и время выдачи'
+                        variant='dark'
+                        type="date"
+                    />
+                </div>
+                <div className='flex gap-5'>
+                    <Button onClick={prev}>Назад</Button>
+                    <Button type='submit'>Создать и отправить</Button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default ServiceInfo;
